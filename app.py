9eaf31019489044fcdc1ec452908b4a084baa8bd
Dat Tran Ebank <ebankubu111444@gmail.com>
2024-11-26 22:17:01 +0700

    Error

--- app.py
+++ app.py
@@ -3,13 +3,6 @@ import os
 
 app = Flask(__name__)
 
-# API để cung cấp dữ liệu GeoJSON
-@app.route('/data/hcm')
-def hcm_data():
-    geojson_path = os.path.join("data", "geojson", "hcm.geojson")
-    with open(geojson_path, 'r', encoding='utf-8') as file:
-        data = file.read()
-    return jsonify(data)
 
 # Trang chính của WebGIS
 @app.route('/')
