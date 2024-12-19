3c65cadaa7f4bdacd01fa60d04f69d04c1466189
Dat Tran Ebank <ebankubu111444@gmail.com>
2024-12-19 10:21:34 +0700

    add VQG_TramChim.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
 // Tạo map và giới hạn phạm vi tại Việt Nam
 const map = L.map('map', {
     center: [14.0583, 108.2772],  // Tọa độ trung tâm của Việt Nam
-    zoom: 7,
+    zoom: 6,
+    minZoom:6,
     maxBounds: [
-        [7, 103],  // Góc dưới trái
-        [25, 110]   // Góc trên phải
+        [5, 102],  // Góc dưới trái
+        [26, 111]   // Góc trên phải
     ],
     maxBoundsViscosity: 1.0 // Đảm bảo rằng các thao tác zoom sẽ giữ trong phạm vi này
 });
@@ -19,79 +20,49 @@ osm.addTo(map);
 /*===================================================
                       GEOJSON
 ===================================================*/
-//var vietnamData = L.geoJSON(vietnamJS).addTo(map);
-//var vietnamData2 = L.geoJSON(vietnamJS2).addTo(map);
-//var provincesData = L.geoJSON(provincesJS).addTo(map);
-//var gadm41Data = L.geoJSON(gadm41JS).addTo(map);
+// Thêm các polygon vào bản đồ
+var vietnamLayer=L.geoJSON(vietnamJS, {
+    onEachFeature: function (feature, layer) {
+        var properties = feature.properties;
 
-// var gadm41Data = L.geoJSON(gadm41JS, {
-//     onEachFeature: function (feature, layer) {
-//         layer.bindPopup(`<b>Name: </b>` + feature.properties.name)
-//     }}).addTo(map)
+        // Gắn sự kiện click để hiển thị thuộc tính
+        layer.on('click', function () {
+            var sidebar = document.getElementById('properties-content');
+            var content = "<b>Thuộc tính:</b><br>";
 
-    // Thêm các polygon vào bản đồ
-// L.geoJSON(gadm41JS, {
-//       onEachFeature: function (feature, layer) {
-//         // Gắn popup hiển thị các thuộc tính của polygon
-//         var properties = feature.properties;
-//         var popupContent = "<b>Thuộc tính:</b><br>";
-//
-//         for (var key in properties) {
-//           popupContent += key + ": " + properties[key] + "<br>";
-//         }
-//
-//         layer.bindPopup(popupContent);
-//       }
-//     }).addTo(map);
+            // Hiển thị thuộc tính của polygon
+            for (var key in properties) {
+                content += key + ": " + properties[key] + "<br>";
+            }
 
-//     // Thêm các polygon vào bản đồ
-// L.geoJSON(vietnamJS, {
-//       onEachFeature: function (feature, layer) {
-//         // Gắn popup hiển thị các thuộc tính của polygon
-//         var properties = feature.properties;
-//         var popupContent = "<b>Thuộc tính:</b><br>";
-//
-//         for (var key in properties) {
-//           popupContent += key + ": " + properties[key] + "<br>";
-//         }
-//
-//         layer.bindPopup(popupContent);
-//       }
-//     }).addTo(map);
-//
-//     // Thêm các polygon vào bản đồ
-// L.geoJSON(provincesJS, {
-//       onEachFeature: function (feature, layer) {
-//         // Gắn popup hiển thị các thuộc tính của polygon
-//         var properties = feature.properties;
-//         var popupContent = "<b>Thuộc tính:</b><br>";
-//
-//         for (var key in properties) {
-//           popupContent += key + ": " + properties[key] + "<br>";
-//         }
-//
-//         layer.bindPopup(popupContent);
-//       }
-//     }).addTo(map);
-//
-//
-//     // Thêm các polygon vào bản đồ
-// L.geoJSON(dongthapJS, {
-//       onEachFeature: function (feature, layer) {
-//         // Gắn popup hiển thị các thuộc tính của polygon
-//         var properties = feature.properties;
-//         var popupContent = "<b>Thuộc tính:</b><br>";
-//
-//         for (var key in properties) {
-//           popupContent += key + ": " + properties[key] + "<br>";
-//         }
-//
-//         layer.bindPopup(popupContent);
-//       }
-//     }).addTo(map);
+            sidebar.innerHTML = content;
 
-    // Thêm các polygon vào bản đồ
-L.geoJSON(dongthap_caphuyenJS, {
+            // Hiển thị sidebar nếu đang ẩn
+            var sidebarContainer = document.getElementById('properties');
+            sidebarContainer.classList.remove('collapsed');
+        });
+
+        // Gắn popup hiển thị các thuộc tính
+        var popupContent = "<b>Thuộc tính:</b><br>";
+        for (var key in properties) {
+            popupContent += key + ": " + properties[key] + "<br>";
+        }
+        layer.bindPopup(popupContent);
+    },
+      style: function (feature) {
+    // Tùy chọn màu sắc và kiểu đường viền cho polygon
+    return {
+      color: "#0067ff",  // Màu đường viền (blue)
+      weight: 3,         // Độ dày đường viền
+      opacity: 1,        // Độ mờ của đường viền
+      fillColor: "#d1ffff",  // Màu nền (light blue)
+      fillOpacity: 0.3   // Độ mờ của màu nền
+    };
+  }
+    }).addTo(map);
+
+// Thêm các polygon vào bản đồ
+var provincesLayer=L.geoJSON(provincesJS, {
       onEachFeature: function (feature, layer) {
         // Gắn popup hiển thị các thuộc tính của polygon
         var properties = feature.properties;
@@ -102,8 +73,147 @@ L.geoJSON(dongthap_caphuyenJS, {
         }
 
         layer.bindPopup(popupContent);
-      }
+      },
+      style: function (feature) {
+    // Tùy chọn màu sắc và kiểu đường viền cho polygon
+    return {
+      color: "#03ad00",  // Màu đường viền (blue)
+      weight: 3,         // Độ dày đường viền
+      opacity: 1,        // Độ mờ của đường viền
+      fillColor: "#fffacd",  // Màu nền (light blue)
+      fillOpacity: 0.3   // Độ mờ của màu nền
+    };
+  }
     }).addTo(map);
+
+// Thêm các polygon vào bản đồ
+var dongthapLayer=L.geoJSON(dongthapJS, {
+      onEachFeature: function (feature, layer) {
+        // Gắn popup hiển thị các thuộc tính của polygon
+        var properties = feature.properties;
+        var popupContent = "<b>Thuộc tính:</b><br>";
+
+        for (var key in properties) {
+          popupContent += key + ": " + properties[key] + "<br>";
+        }
+
+        layer.bindPopup(popupContent);
+      },
+      style: function (feature) {
+    // Tùy chọn màu sắc và kiểu đường viền cho polygon
+    return {
+      color: "#000000",  // Màu đường viền (blue)
+      weight: 3,         // Độ dày đường viền
+      opacity: 1,        // Độ mờ của đường viền
+      fillColor: "#c4c4c4",  // Màu nền (light blue)
+      fillOpacity: 0.3   // Độ mờ của màu nền
+    };
+  }
+    }).addTo(map);
+
+// Thêm các polygon vào bản đồ
+var dongthapCaphuyenLayer=L.geoJSON(dongthap_caphuyenJS, {
+      onEachFeature: function (feature, layer) {
+        // Gắn popup hiển thị các thuộc tính của polygon
+        var properties = feature.properties;
+        var popupContent = "<b>Thuộc tính:</b><br>";
+
+        for (var key in properties) {
+          popupContent += key + ": " + properties[key] + "<br>";
+        }
+
+        layer.bindPopup(popupContent);
+      },
+      style: function (feature) {
+    // Tùy chọn màu sắc và kiểu đường viền cho polygon
+    return {
+      color: "#ff0000",  // Màu đường viền (blue)
+      weight: 3,         // Độ dày đường viền
+      opacity: 1,        // Độ mờ của đường viền
+      fillColor: "#a4a4a4",  // Màu nền (light blue)
+      fillOpacity: 0.3   // Độ mờ của màu nền
+    };
+  }
+    }).addTo(map);
+
+// Thêm các polygon vào bản đồ
+var tramchimLayer=L.geoJSON(tramchimJS, {
+      onEachFeature: function (feature, layer) {
+        // Gắn popup hiển thị các thuộc tính của polygon
+        var properties = feature.properties;
+        var popupContent = "<b>Thuộc tính:</b><br>";
+
+        for (var key in properties) {
+          popupContent += key + ": " + properties[key] + "<br>";
+        }
+
+        layer.bindPopup(popupContent);
+      },
+      style: function (feature) {
+    // Tùy chọn màu sắc và kiểu đường viền cho polygon
+    return {
+      color: "#ff7e00",  // Màu đường viền (blue)
+      weight: 3,         // Độ dày đường viền
+      opacity: 1,        // Độ mờ của đường viền
+      fillColor: "#2e90ff",  // Màu nền (light blue)
+      fillOpacity: 0.3   // Độ mờ của màu nền
+    };
+  }
+    }).addTo(map);
+
+
+
+/*===================================================
+               Turn on/off title layers
+===================================================*/
+document.getElementById("Open Street Map").addEventListener("change", function(e) {
+    if (e.target.checked) {
+        osm.addTo(map);
+    } else {
+        map.removeLayer(osm);
+    }
+});
+
+document.getElementById("vqgtramchim").addEventListener("change", function(e) {
+    if (e.target.checked) {
+        tramchimLayer.addTo(map);
+    } else {
+        map.removeLayer(tramchimLayer);
+    }
+});
+
+document.getElementById("dongthap_caphuyen").addEventListener("change", function(e) {
+    if (e.target.checked) {
+        dongthapCaphuyenLayer.addTo(map);
+    } else {
+        map.removeLayer(dongthapCaphuyenLayer);
+    }
+});
+
+document.getElementById("dongthap").addEventListener("change", function(e) {
+    if (e.target.checked) {
+        dongthapLayer.addTo(map);
+    } else {
+        map.removeLayer(dongthapLayer);
+    }
+});
+
+document.getElementById("provinces").addEventListener("change", function(e) {
+    if (e.target.checked) {
+        provincesLayer.addTo(map);
+    } else {
+        map.removeLayer(provincesLayer);
+    }
+});
+
+document.getElementById("vietnam").addEventListener("change", function(e) {
+    if (e.target.checked) {
+        vietnamLayer.addTo(map);
+    } else {
+        map.removeLayer(vietnamLayer);
+    }
+});
+
 
 // Sidebar Properties toggle
 const properties = document.getElementById('properties');
@@ -151,3 +261,5 @@ toggleDashboardBtn.addEventListener('click', () => {
     // Cập nhật vị trí nút zoom
     updateZoomControlPosition();
 });
+
+
